b1e0ab74f15a6033d2bf86ed868e97a849303e47
ThatRandomAdmin <thatrandomdev@gmail.com>
2021-12-10 16:49:22 +0000

    Question 1 of Day 2

---
+++ Day_2/q1p1.py
@@ -0,0 +1,18 @@
+f = open("Day_2\input.txt", "r")
+lines = f.readlines()
+
+x_axis = 0
+y_axis = 0
+
+for i in lines:
+    command, amount = i.split()
+    amount = int(amount)
+
+    if command == "up":
+        y_axis -= amount
+    elif command == "down":
+        y_axis += amount
+    else:
+        x_axis += amount
+
+print(x_axis * y_axis)  
